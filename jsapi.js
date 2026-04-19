9c7c1eb833c2632aa0dccb9c0b85a59fe21c60d5
iCloud oof <shtoolgsm@gmail.com>
2026-04-19 17:12:17 +0300

    Delete jsapi.js

--- jsapi.js
+++
@@ -1,6 +0,0 @@
-function sendOrder(imei, device, service, currency){
-    fetch(`api/order.php?imei=${imei}&device=${device}&service=${service}&currency=${currency}`)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
-}
